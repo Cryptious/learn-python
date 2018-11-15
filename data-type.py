0b536ab8a9490f0dfebd2ae9e2b39ec6cf5b069e
Martino Wagey <martinowagey@gmail.com>
2018-11-15 10:51:23 +0700

    Tipe Data

---
+++ data-type.py
@@ -0,0 +1,48 @@
+angka1 = 10
+angka2 = 3.14
+angka3 = 1000000000000000000000
+angka4 = 10+5j
+karakter = 'A'
+kalimat = "Lorem ipsum sit dolor amet"
+is_admin = True
+is_member = False
+daftar_angka = [1, 2, 3]
+daftar_huruf = ('a', 'b', 'c')
+data_orang = {'name':'michael', 'country':'Indonesia', 'phone':'7501234'}
+operator = 1+2
+
+print (angka1)
+print (type(angka1))
+
+print (angka2)
+print (type(angka2))
+
+print (angka3)
+print (type(angka3))
+
+print (angka4)
+print (type(angka4))
+
+print (karakter)
+print (type(karakter))
+
+print (kalimat)
+print (type(kalimat))
+
+print (is_admin)
+print (type(is_admin))
+
+print (is_member)
+print (type(is_member))
+
+print (daftar_angka)
+print (type(daftar_angka))
+
+print (daftar_huruf)
+print (type(daftar_huruf))
+
+print (data_orang)
+print (type(data_orang))
+
+print (operator)
+print (type(operator))
