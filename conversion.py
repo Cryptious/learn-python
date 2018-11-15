452597a7ad63c064f81dbf51392b7ef133452217
Martino Wagey <martinowagey@gmail.com>
2018-11-15 11:15:58 +0700

    conversion

---
+++ conversion.py
@@ -0,0 +1,19 @@
+# Suatu tipe data dapat dikonversi ke tipe data lain yang berbeda menggunakan beberapa function berikut
+
+angka = 180
+
+var1 = unichr(angka)
+var2 = float(angka)
+var3 = complex(angka)
+var4 = hex(angka)
+var5 = oct(angka)
+
+print (var1)
+print (var2)
+print (var3)
+print (var4)
+print (var5)
+
+# Selain diatas, ada juga func lain seperti
+# chr(), mengubah angka ke karakter
+# int(), mengubah tipe data lain ke angka
