9e502197e5128358e1a42157579ab58502ac45da
Martino Wagey <martinowagey@gmail.com>
2018-11-16 15:39:44 +0700

    Update

--- operator-logika.py
+++
@@ -1,8 +0,0 @@
-x = 12.0
-y = 5.0
-
-print (" x == y ", x == y)
-print (" x != y ", x != y)
-print (" x == y AND x != y ", (x == y and x != y) ) # Operator AND akan bernilai BENAR apabila kedua nilai adalah TRUE
-print (" x == y OR x != y ", (x == y or x != y) ) # Operator OR akan bernilai BENAR apabila salah satu nilai adalah TRUE
-print (" NOT (x == y) ", not(x == y) ) # Operator NOT bernilah negatif/ menjadi lawan nya, TRUE menjadi FALSE
